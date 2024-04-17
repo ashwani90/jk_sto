a74acf26cee9301995d26adab06ed9b7b0bee71e
Ashwani <ashwani90125@gmail.com>
2024-04-17 12:11:51 +0530

    Work on starting chat module

--- jkinda_stocks/static/js/chat.js
+++ jkinda_stocks/static/js/chat.js
@@ -22,7 +22,7 @@ $(".modal-close-button").on("click", function() {
 })
 
 $("#send-message").click(() => {
-    let message = $("#message-input").val();
+   /* let message = $("#message-input").val();
     let data = {
         message: message
     };
@@ -43,5 +43,53 @@ $("#send-message").click(() => {
             </div>`;
             $("#chat-box-content").append(reply);
         } 
-    }})
+    }})*/
+})
+
+$(document).ready(function() {
+    let socket = new WebSocket(
+        `ws://127.0.0.1:8000/ws/users/3/chat/`
+      );
+      
+      $("#send-message").click(() => {
+        // let message = $("#message-input").val();
+        const msg = {
+            type: "message",
+            text: document.getElementById("message-input").value,
+            id: clientID,
+            date: Date.now(),
+          };
+        let sent = `<div class="chat-box-body-send">
+                    <p>${message}</p>
+                    <span>12:00</span>
+                </div>`;
+                $("#chat-box-content").append(sent);
+        $("#message-input").val("");
+        // add message to the list
+        // let data_url = window.main_url+"chat/send/";
+        // $.post({url: data_url,data:data, success: (result) => {
+        //     if (result.success) {
+                
+        //         let reply = `<div class="chat-box-body-receive">
+        //             <p>${result.reply}</p>
+        //             <span>12:00</span>
+        //         </div>`;
+        //         $("#chat-box-content").append(reply);
+        //     } 
+        // }})
+        socket.send(JSON.stringify(msg));
+    })
+
+      socket.onopen = (event) => {
+        const msg = {
+            type: "message",
+            text: document.getElementById("message-input").value,
+            id: clientID,
+            date: Date.now(),
+          };
+          socket.send(JSON.stringify(msg));
+      };
+      socket.onmessage = (event) => {
+        console.log(event.data);
+      };
 })
